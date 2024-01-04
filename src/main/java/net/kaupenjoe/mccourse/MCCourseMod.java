1bd4620cbbd1865b85c18a0aa811da53521effde
Pintér Gábor <pinter.gabor@gmx.at>
2024-01-04 09:46:31 +0100

    mc1.20.4, no mixins

--- src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
+++ src/main/java/net/kaupenjoe/mccourse/MCCourseMod.java
@@ -1,19 +1,17 @@
 package net.kaupenjoe.mccourse;
 
-import net.fabricmc.api.ModInitializer;
-
 import net.kaupenjoe.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.fabricmc.api.ModInitializer;
+
 public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
 		ModItems.registerModItems();
-
 	}
 }
