303a57fe8a374b983312ad993018bf0211837a6b
Pintér Gábor <pinter.gabor@gmx.at>
2024-01-04 17:36:32 +0100

    +recipes

--- src/main/java/net/kaupenjoe/mccourse/datagen/ModRecipeGenerator.java
+++ src/main/java/net/kaupenjoe/mccourse/datagen/ModRecipeGenerator.java
@@ -7,9 +7,10 @@ import net.kaupenjoe.mccourse.item.ModItems;
 
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
-import net.minecraft.util.Identifier;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
@@ -29,7 +30,7 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 			.input('P', ModItems.PINK_GARNET)
 			.criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
 			.criterion(hasItem(ModItems.PINK_GARNET), conditionsFromItem(ModItems.PINK_GARNET))
-			.offerTo(exporter, new Identifier(getRecipeName(ModItems.RAW_PINK_GARNET) + "_"));
+			.offerTo(exporter);
 
 		offerReversibleCompactingRecipes(exporter,
 			RecipeCategory.MISC, ModItems.PINK_GARNET,
@@ -40,10 +41,36 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
 				ModBlocks.NETHER_PINK_GARNET_ORE, ModBlocks.END_STONE_PINK_GARNET_ORE),
 			RecipeCategory.MISC, ModItems.PINK_GARNET,
 			0.25f, 200, "pink_garnet");
+
 		offerBlasting(exporter,
 			List.of(ModItems.RAW_PINK_GARNET, ModBlocks.PINK_GARNET_ORE, ModBlocks.DEEPSLATE_PINK_GARNET_ORE,
 				ModBlocks.NETHER_PINK_GARNET_ORE, ModBlocks.END_STONE_PINK_GARNET_ORE),
 			RecipeCategory.MISC, ModItems.PINK_GARNET,
 			0.25f, 200, "pink_garnet");
+
+		createSlabRecipe(
+			RecipeCategory.BUILDING_BLOCKS,
+			ModBlocks.PINK_GARNET_SLAB,
+			Ingredient.ofItems(ModItems.PINK_GARNET))
+			.criterion(hasItem(ModItems.PINK_GARNET), conditionsFromItem(ModItems.PINK_GARNET))
+			.offerTo(exporter);
+
+		createStairsRecipe(
+			ModBlocks.PINK_GARNET_STAIRS,
+			Ingredient.ofItems(ModItems.PINK_GARNET))
+			.criterion(hasItem(ModItems.PINK_GARNET), conditionsFromItem(ModItems.PINK_GARNET))
+			.offerTo(exporter);
+
+		ShapelessRecipeJsonBuilder.create(RecipeCategory.REDSTONE, ModBlocks.PINK_GARNET_BUTTON)
+			.input(ModItems.PINK_GARNET)
+			.criterion(hasItem(ModItems.PINK_GARNET), conditionsFromItem(ModItems.PINK_GARNET))
+			.offerTo(exporter);
+
+		createPressurePlateRecipe(
+			RecipeCategory.REDSTONE,
+			ModBlocks.PINK_GARNET_PRESSURE_PLATE,
+			Ingredient.ofItems(ModItems.PINK_GARNET))
+			.criterion(hasItem(ModItems.PINK_GARNET), conditionsFromItem(ModItems.PINK_GARNET))
+			.offerTo(exporter);
 	}
 }
